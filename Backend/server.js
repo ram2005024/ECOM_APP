3a236ecc570cc4beccd9d16622693a571e220b0f
Ram Sharma <sharmashekhar20050@gmail.com>
2026-01-13 21:48:16 +0545

    Final

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 //middlewares
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
