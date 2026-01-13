b76e28bcd5dc0add4254ab1ce7fdd3497b844cc6
Ram Sharma <sharmashekhar20050@gmail.com>
2026-01-13 22:56:41 +0545

    Final

--- Backend/controllers/passportController.js
+++ Backend/controllers/passportController.js
@@ -3,16 +3,19 @@ import { generateToken } from "../libs/userAuthHelper.js";
 // For google redirect
 export const googleAuthController = async (req, res) => {
   const user = req.user;
+  console.log("Bro", user);
   try {
     if (!user)
       return res.status(500).json({ message: "Server error", success: false });
     const token = await generateToken(user);
+
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 1 * 24 * 60 * 60 * 1000,
     });
+    user.password = undefined;
     return res.redirect(process.env.CLIENT_URL);
   } catch (error) {
     console.log(error);
