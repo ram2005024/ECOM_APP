ea9db48d3c6867ba1696e73e8d86d30cc8eb4dba
Ram Sharma <sharmashekhar20050@gmail.com>
2026-01-14 11:43:22 +0545

    Uncomment eventKey and add baseUrl to Inngest

--- Backend/inngest/client.js
+++ Backend/inngest/client.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 export const inngest = new Inngest({
   id: "ECOM_APP",
-  // eventKey: process.env.INNGEST_EVENT_KEY,
+  eventKey: process.env.INNGEST_EVENT_KEY,
+    baseUrl: "https://ecom-app-1-lte5.onrender.com",
 });
