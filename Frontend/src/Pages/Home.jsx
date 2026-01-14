e66e89febc09883b440a1dae2565c0d6fd1ae3ff
Ram Sharma <sharmashekhar20050@gmail.com>
2026-01-14 09:51:03 +0545

    Implement user authentication in Home component
    Add user authentication on Home page using Redux and Axios.

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -4,8 +4,26 @@ import Product from "../Components/Home/Product";
 import ScrollProduct from "../Components/Home/ScrollProduct";
 import Selling from "../Components/Home/Selling";
 import Specification from "../Components/Home/Specification";
-
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { login } from "../slices/authSlice";
 const Home = () => {
+    const dispatch = useDispatch();
+  useEffect(() => {
+    try {
+      (async () => {
+        const res = await axios.get(
+          import.meta.env.VITE_SERVER_URL + "/auth/user/me",
+          {
+            withCredentials: true,
+          }
+        );
+        dispatch(login(res.data.user));
+      })();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [dispatch]);
   return (
     <div>
       <hr className="text-gray-300 " />
