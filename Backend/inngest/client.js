0a2e57e0dda3c5d15d8c52a84268aa1235e8cb4a
Ram Sharma <sharmashekhar20050@gmail.com>
2026-01-14 12:01:26 +0545

    Update Inngest configuration to use signingKey

--- Backend/inngest/client.js
+++ Backend/inngest/client.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 export const inngest = new Inngest({
   id: "ECOM_APP",
-  eventKey: process.env.INNGEST_EVENT_KEY,
+  signingKey: process.env.INNGEST_SIGNING_KEY,
     baseUrl: "https://ecom-app-1-lte5.onrender.com",
 });
