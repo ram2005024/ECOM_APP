565b69a7c41ee892ea078a4bf91512a190975320
Ram Sharma <sharmashekhar20050@gmail.com>
2026-01-15 13:42:40 +0545

    Add eventKey to Inngest client configuration

--- Backend/inngest/client.js
+++ Backend/inngest/client.js
@@ -2,5 +2,6 @@ import { Inngest } from "inngest";
 export const inngest = new Inngest({
   id: "ECOM_APP",
   signingKey: process.env.INNGEST_SIGNING_KEY,
+  eventKey:process.env.INNGEST_EVENT_KEY,
     baseUrl: "https://ecom-app-1-lte5.onrender.com",
 });
